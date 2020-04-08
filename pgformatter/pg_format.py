6daf160db9bf70221e455b250d64e3c4fba39ae6
Nathan Cahill <nathan@nathancahill.com>
2020-04-08 10:25:19 +0200

    error handling

--- pgformatter/pg_format.py
+++ pgformatter/pg_format.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import subprocess
 from os import path
 
@@ -11,4 +12,12 @@ def pg_format(sql: bytes):
     if os.environ.get("LAMBDA_TASK_ROOT") and os.environ.get("NOW_REGION") != "dev1":
         cmd = path.join(package_directory, "..", "bin", "lambda", "pg_format")
 
-    return subprocess.check_output(cmd, input=sql)
+    output = ""
+
+    try:
+        output = subprocess.check_output(cmd, input=sql, stderr=subprocess.PIPE)
+    except subprocess.CalledProcessError as e:
+        print(e.stderr.decode(sys.getfilesystemencoding()))
+        raise e
+
+    return output
